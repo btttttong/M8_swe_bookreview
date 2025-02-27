cc7ce0df6f68b15604afa138f8e6c26ae168ec87
supakavadee r <tong.7147@gmail.com>
2025-02-27 23:10:59 +0700

    init

---
+++ src/main/java/com/example/BookReviewSystem/BookReviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookReviewSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookReviewSystem/BookReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookReviewSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
